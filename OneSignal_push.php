0544af7b85ce873aafe6ad7d963687a151d64225
GinatheDog <44173551+GinatheDog@users.noreply.github.com>
2019-05-01 09:52:42 -0600

    Update OneSignal_push.php

--- OneSignal_push.php
+++ OneSignal_push.php
@@ -2,45 +2,45 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<title>Notifications</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Notifications</title>
     <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="../assets/js/bootstrap.min.js"></script>
-	<style>
-		#code_area{
-			display:none;			
-		}
-		#message{
-			opacity: 0.5;			
-		}
-		body { 
-		  background: url(../assets/photo/background.jpg) no-repeat center center fixed;
-		  -webkit-background-size: cover;
-		  -moz-background-size: cover;
-		  -o-background-size: cover;
-		  background-size: cover;
-		}
-	</style>
+    <style>
+	#code_area{
+		display:none;			
+	}
+	#message{
+		opacity: 0.5;			
+	}
+	body { 
+	  background: url(../assets/photo/background.jpg) no-repeat center center fixed;
+	  -webkit-background-size: cover;
+	  -moz-background-size: cover;
+	  -o-background-size: cover;
+	  background-size: cover;
+	}
+    </style>
 </head>
 
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
-			</div>
-			<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-				<br><br>
-				<form id="myForm" action="AppActivateERP.php" method="post">
-				    <textarea class="form-control" id="message" name="message" rows="18" placeholder ="Please insert your message here..."></textarea>
-				    <br>
-				    <button type="button" onclick="postTimeEvent();" class="btn btn-success btn-lg btn-block">Send ERP Activation</button>
-				</form>
-			</div>
-			<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+				</div>
+						<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+							<br><br>
+							<form id="myForm" action="AppActivateERP.php" method="post">
+									<textarea class="form-control" id="message" name="message" rows="18" placeholder ="Please insert your message here..."></textarea>
+									<br>
+									<button type="button" onclick="postTimeEvent();" class="btn btn-success btn-lg btn-block">Send ERP Activation</button>
+							</form>
+						</div>
+					<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+				</div>
 			</div>
 		</div>
-	</div>
 
 	<div id = 'code_area'>
 		<?php								
